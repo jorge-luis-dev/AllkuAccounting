3d923f5ada7f65d9a090dde346a01fc40a0eeabe
Jorge Luis <jorgequiguango@yahoo.com>
2020-11-09 12:53:32 -0500

    Added relation ship cascade

--- bookapi.py
+++ bookapi.py
@@ -6,33 +6,43 @@ from app import db
 
 parser = reqparse.RequestParser()
 
+
 class BookListApi(Resource):
+
     def get(self):
         try:
             books = Book.query.all()
-            return jsonify([e.to_json() for e in books])
+            return jsonify([b.serialize() for b in books])
         except Exception as e:
             return str(e), 404
 
     def post(self):
-        parser.add_argument('name', type=str, help="Name of the book", required=True)
+        parser.add_argument('name', type=str,
+                            help="Name of the book",
+                            required=True)
+        parser.add_argument('editorial')
         parser.add_argument('published')
         args = parser.parse_args()
         name = args['name']
+        editorial = args['editorial']
         published = args['published']
         try:
-            book = Book(name=name, published=published, authors=None)
+            book = Book(name=name, editorial=editorial ,published=published)
             db.session.add(book)
             db.session.commit()
             return "Book added. book id={}".format(book.id), 201
         except Exception as e:
             return str(e), 400
 
+
 class BookApi(Resource):
+
     def get(self, id_book):
         try:
-           book = Book.query.filter_by(id=id_book).first()
-           return jsonify(book.serialize())
+            book = Book.query.filter_by(id=id_book).first()
+            if book is None:
+                return None, 404
+            return jsonify(book.serialize())
         except Exception as e:
             return str(e), 404
 
@@ -58,10 +68,11 @@ class BookApi(Resource):
 
     def delete(self, id_book):
         try:
-           book = Book.query.filter_by(id=id_book).first()
-           db.session.delete(book)
-           db.session.commit()
-           return jsonify(book.serialize())
+            book = Book.query.filter_by(id=id_book).first()
+            if book is None:
+                return None, 404
+            db.session.delete(book)
+            db.session.commit()
+            return jsonify(book.serialize())
         except Exception as e:
             return str(e), 404
-
--- models.py
+++ models.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from app import db
-from flask import jsonify
 
 
 class Book(db.Model):
@@ -10,52 +9,44 @@ class Book(db.Model):
     name = db.Column(db.String(100), nullable=False)
     editorial = db.Column(db.String(150), nullable=False)
     published = db.Column(db.String(100), nullable=False)
-    authors = db.relationship('Author', backref='book', lazy='dynamic')
+    authors = db.relationship('Author',
+                              backref='book',
+                              lazy='dynamic',
+                              cascade="all, delete")
 
     def __init__(self, name, editorial, published):
         self.name = name
-        self.editorial = author
+        self.editorial = editorial
         self.published = published
 
-    def to_json(self):
-        
-        res_data = []
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        authors_data = []
         for a in self.authors.all():
-            tmp = {}
-            tmp['id']=a.id
-            tmp['name']=a.name
-            res_data.append(tmp)
-            print(a.id)
-            print(a.name)
+            a_data = {}
+            a_data['id'] = a.id
+            a_data['name'] = a.name
+            authors_data.append(a_data)
 
-
-        json_book = {
+        book_data = {
             'id': self.id,
             'name': self.name,
             'editorial': self.editorial,
             'published': self.published,
-            'authors': res_data
+            'authors': authors_data
         }
-        return json_book
+        return book_data
 
-    # def __repr__(self):
-    #     return '<id {}>'.format(self.id)
-
-    # def serialize(self):
-    #     return {
-    #         'id': self.id,
-    #         'name': self.name,
-    #         'author': self.author,
-    #         'published': self.published
-    #     }
-
-
+    
 class Author(db.Model):
     __tablename__ = 'authors'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    book_id = db.Column(db.Integer, db.ForeignKey('books.id'))
+    book_id = db.Column(db.Integer,
+                        db.ForeignKey('books.id', ondelete='CASCADE'))
 
     def __init__(self, name):
         self.name = name
